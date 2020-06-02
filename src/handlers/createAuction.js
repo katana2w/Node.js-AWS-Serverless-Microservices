542096a23e1ab38f4f6beb5f757905fc3baaa092
Yuri Kulyk <katana_2w@ukr.net>
2020-06-02 17:16:36 +0400

    add new endpoints & update serverless config

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,28 +1,41 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import commonMiddleware from '../lib/commonMiddleware';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const creationDate = new Date();
+
+
 
   const auction = {
     id: uuid(),
     title,
     status: 'OPEN',
-    createdAt: creationDate.toISOString()
+    createdAt: creationDate.toISOString(),
+    highestBid: {
+      amount: 0
+    }
   };
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction
-  }).promise();
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise();
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
   };
 }
 
-export const handler = createAuction;
+export const handler = commonMiddleware(createAuction);
 
 
